5e0e88022608a710d2cd3f5b249c6f413fe438b0
hellollw <hiluliwen@gmail.com>
2019-11-23 22:11:51 +0800

    Basic

---
+++ ch3_7.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# Author: Lu Liwen
+# Modified Time: 2019-11-23
+
+"""
+P72 第七题
+"""
+from lib import Basic_Transform
+from numpy import *
+
---
+++ lib/Basic_Transform.py
@@ -0,0 +1,65 @@
+# -*- coding:utf-8 -*-
+# Author: Lu Liwen
+# Modified Time: 2019-11-23
+
+"""
+常用的旋转计算
+
+包含方法：
+1. 四元数——>旋转矩阵 quaternion_to_RotationMatrix(quaternion)
+2. 构造位姿矩阵T：def constructT(R, t)
+3. 四元数归一化：def normalize_q(q)
+
+"""
+
+from numpy import *
+
+
+# 四元数——>旋转矩阵
+# 输入：四元数
+# 输出：对应的旋转矩阵
+def quaternion_to_RotationMatrix(quaternion):
+    q = quaternion.copy()
+    if shape(q) == (1, 4):  # shape返回元组
+        q = q.A[0]  # 转换为一维数组
+        RotationMatrix = mat(
+            [[1 - 2 * q[2] ** 2 - 2 * q[3] ** 2, 2 * q[1] * q[2] + 2 * q[0] * q[3], 2 * q[1] * q[3] - 2 * q[0] * q[2]],
+             [2 * q[1] * q[2] - 2 * q[
+                 0] * q[3], 1 - 2 * q[1] ** 2 - 2 * q[3] ** 2, 2 * q[2] * q[3] + 2 * q[0] * q[1]],
+             [2 * q[1] * q[3] + 2 * q[0] * q[2], 2 * q[2] * q[3] - 2 * q[0] * q[1],
+              1 - 2 * q[1] ** 2 - 2 * q[2] ** 2]])
+        return RotationMatrix
+    else:
+        raise SystemError('四元数格式不正确')
+
+
+# 构造位姿矩阵
+# 输入：旋转矩阵:R, 平移向量:t(1行三列）
+# 输出：位姿矩阵:T
+def constructT(R, t):
+    if shape(R) == (3,3) and shape(t)==(1,3):
+        T = mat(zeros((4, 4)))
+        T[0:3,0:3] = R.copy()   #stop不包括在内
+        T[0:3,3] = t.T.copy()
+        T[3,0:3] = 0
+        T[3,3] = 1
+        return T
+    else:
+        raise SystemError('输入格式不正确')
+
+# 四元数归一化（只有单位四元数（四元数模长为1）才表示旋转）——>每一项都除以总和的均值
+# 输入：未归一化的四元数:q
+# 输出：归一化的四元数:q_normal
+def normalize_q(q):
+    q_normal = q.copy()
+    q_sum = sum(q)
+    q_normal = q_normal/q_sum
+    return q_normal
+
+
+if __name__ == '__main__':
+    q = mat([0, 1, 2, 3])
+    t = mat([1,2,3])
+    R = quaternion_to_RotationMatrix(q)
+    T = constructT(R,t)
+    print(T)
