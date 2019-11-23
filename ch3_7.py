33d1783bcf6a7446b12f200504e4e1ab764a446d
hellollw <hiluliwen@gmail.com>
2019-11-23 22:54:30 +0800

    Basic

--- ch3_7.py
+++ ch3_7.py
@@ -5,6 +5,23 @@
 """
 P72 第七题
 """
-from lib import Basic_Transform
+from lib import Basic_Transform as Basic
 from numpy import *
 
+if __name__ == '__main__':
+    q1 = mat([0.35, 0.2, 0.3, 0.1])
+    t1 = mat([0.3, 0.1, 0.1])
+    q2 = mat([-0.5, 0.4, -0.1, 0.2])
+    t2 = mat([-0.1, 0.5, 0.3])
+    # 构建旋转矩阵
+    R1 = Basic.quaternion_to_RotationMatrix(Basic.normalize_q(q1))
+    R2 = Basic.quaternion_to_RotationMatrix(Basic.normalize_q(q2))
+    # 构建位姿矩阵(从世界坐标系到相机坐标系的转换）
+    Tcw1 = Basic.constructT(R1, t1)
+    Tcw2 = Basic.constructT(R2, t2)
+    # 求解在小罗卜二号坐标系下的坐标
+    p = mat([0.5,0,0.2]).T
+    pc1 = Basic.homoLocation(p)
+    pw = Tcw1.I*pc1
+    pc2 = Tcw2*pw
+    print(pc2)
--- lib/Basic_Transform.py
+++ lib/Basic_Transform.py
@@ -3,16 +3,31 @@
 # Modified Time: 2019-11-23
 
 """
+格式：
+行向量表示（平移向量，四元数）
+列向量表示（坐标）
+
 常用的旋转计算
 
 包含方法：
 1. 四元数——>旋转矩阵 quaternion_to_RotationMatrix(quaternion)
 2. 构造位姿矩阵T：def constructT(R, t)
 3. 四元数归一化：def normalize_q(q)
+4. 构建齐次坐标：def homoLocation(p)
 
 """
 
-from numpy import *
+import numpy as num
+
+
+# 构建其次坐标([p
+#              1])
+# 输入：三维坐标p
+# 输出: 齐次坐标p_home
+def homoLocation(p):
+    p_homo = num.mat(num.ones((4, 1)))
+    p_homo[0:3,0] = p.copy()
+    return p_homo
 
 
 # 四元数——>旋转矩阵
@@ -20,9 +35,9 @@ from numpy import *
 # 输出：对应的旋转矩阵
 def quaternion_to_RotationMatrix(quaternion):
     q = quaternion.copy()
-    if shape(q) == (1, 4):  # shape返回元组
+    if num.shape(q) == (1, 4):  # shape返回元组
         q = q.A[0]  # 转换为一维数组
-        RotationMatrix = mat(
+        RotationMatrix = num.mat(
             [[1 - 2 * q[2] ** 2 - 2 * q[3] ** 2, 2 * q[1] * q[2] + 2 * q[0] * q[3], 2 * q[1] * q[3] - 2 * q[0] * q[2]],
              [2 * q[1] * q[2] - 2 * q[
                  0] * q[3], 1 - 2 * q[1] ** 2 - 2 * q[3] ** 2, 2 * q[2] * q[3] + 2 * q[0] * q[1]],
@@ -37,15 +52,16 @@ def quaternion_to_RotationMatrix(quaternion):
 # 输入：旋转矩阵:R, 平移向量:t(1行三列）
 # 输出：位姿矩阵:T
 def constructT(R, t):
-    if shape(R) == (3,3) and shape(t)==(1,3):
-        T = mat(zeros((4, 4)))
-        T[0:3,0:3] = R.copy()   #stop不包括在内
-        T[0:3,3] = t.T.copy()
-        T[3,0:3] = 0
-        T[3,3] = 1
+    if num.shape(R) == (3, 3) and num.shape(t) == (1, 3):
+        T = num.mat(num.zeros((4, 4)))
+        T[0:3, 0:3] = R.copy()  # stop不包括在内
+        T[0:3, 3] = t.T.copy()
+        T[3, 0:3] = 0
+        T[3, 3] = 1
         return T
     else:
         raise SystemError('输入格式不正确')
+
 
 # 四元数归一化（只有单位四元数（四元数模长为1）才表示旋转）——>每一项都除以总和的均值
 # 输入：未归一化的四元数:q
@@ -53,13 +69,13 @@ def constructT(R, t):
 def normalize_q(q):
     q_normal = q.copy()
     q_sum = sum(q)
-    q_normal = q_normal/q_sum
+    q_normal = q_normal / q_sum
     return q_normal
 
 
-if __name__ == '__main__':
-    q = mat([0, 1, 2, 3])
-    t = mat([1,2,3])
-    R = quaternion_to_RotationMatrix(q)
-    T = constructT(R,t)
-    print(T)
+# if __name__ == '__main__':
+#     q = num.mat([0, 1, 2, 3])
+#     t = num.mat([1, 2, 3])
+#     R = quaternion_to_RotationMatrix(q)
+#     T = constructT(R, t)
+#     print(T)
